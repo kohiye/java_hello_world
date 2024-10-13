be6c1dcf8f5b3a7be590d9869bacc7f9fc235e67
Ivan <procciy@proton.me>
2024-10-13 17:01:47 +0700

    Lab 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,62 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
-    static double add(double a, double b){
-        return a + b;
+    static List<String> fizzbuzz(){
+        List<String> array = new ArrayList<String>();
+        for(int i=1; i <= 500; ++i) {
+            if(i%5 == 0) {
+                if(i%7 == 0){
+                    array.add("fizzbuzz");
+                }else{
+                    array.add("fizz");
+                }
+            }else if(i%7 == 0){
+                array.add("buzz");
+            }else{
+                array.add(String.valueOf(i));
+            }
+        }
+        return array;
+    }
+    static String invert(){
+        String str = "make install";
+        StringBuilder strbuiid = new StringBuilder();
+        for(int i = 0; i < str.length(); ++i){
+            strbuiid.append(str.charAt(str.length() - i - 1));
+        }
+        return strbuiid.toString();
+    }
+    static String quadraticRoots(double a, double b, double c){
+        double discriminant = Math.pow(b,2) - 4 * a * c;
+        if(discriminant < 0){
+            return "No Real Solutions";
+        }
+        return String.valueOf((-b - Math.sqrt(discriminant)) / (2 * a)) + " "
+        + String.valueOf((-b + Math.sqrt(discriminant)) / (2 * a));
+    }
+    static boolean palindrome(String str){
+        int length = str.length();
+        for(int i = 0; str.charAt(i) == str.charAt(length - i - 1) ; ++i){
+            if (i >= length - i - 1)
+                return true;
+        }
+        return false;
+    }
+    static double series(){
+        double sum = 0;
+        int i = 2;
+        double member = 0;
+        do {
+            member = 1 / ( Math.pow(i, 2) + i - 2);
+            sum += member;
+            ++i;
+        }while(member > 10e-6d);
+        return sum;
     }
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,13 +1,37 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
     @Test
-    void add() {
-        assertEquals(4, Main.add(2,2));
+    void fizzbuzz(){
+        List<String> arr = Main.fizzbuzz();
+        assertEquals("1", arr.getFirst());
+        assertEquals("fizz", arr.get(4));
+        assertEquals("buzz", arr.get(6));
+        assertEquals("fizzbuzz", arr.get(34));
+    }
+    @Test
+    void invert(){
+        assertEquals("llatsni ekam", Main.invert());
+    }
+    @Test
+    void quadraticRoots(){
+        assertEquals("No Real Solutions", Main.quadraticRoots(1, 1, 9));
+        assertEquals("-2.0 3.0", Main.quadraticRoots(1, -1, -6));
+    }
+    @Test
+    void palindrome(){
+        assert (Main.palindrome("aboba"));
+        assert (Main.palindrome("torrot"));
+        assert (!Main.palindrome("not pali"));
+    }
+    @Test
+    void series(){
+        assert ( 0.607955 < Main.series() && Main.series() < 0.607957 );
     }
 }
